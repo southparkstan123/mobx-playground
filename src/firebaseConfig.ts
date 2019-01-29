d91a2b51d050a09189a1af342ec1b3776e4cf1fe
southparkstan123 <southparkstan123@gmail.com>
2019-01-29 21:07:37 +0800

    delete firebaseConfig file

--- src/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC-bKGiAOHKowoDxwBXbp3ZMiIF9JlNhYM",
-    authDomain: "mobx-playground.firebaseapp.com",
-    databaseURL: "https://mobx-playground.firebaseio.com",
-    projectId: "mobx-playground",
-    storageBucket: "mobx-playground.appspot.com",
-    messagingSenderId: "410618519388"
-};
